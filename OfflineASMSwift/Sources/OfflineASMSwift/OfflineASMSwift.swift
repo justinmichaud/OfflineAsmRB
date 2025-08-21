263064ff2f8d181dc71a14b01175dd772f48b887
Justin Michaud <jmichaud@igalia.com>
2025-08-21 14:13:20 -0600

    .

---
+++ OfflineASMSwift/Sources/OfflineASMSwift/OfflineASMSwift.swift
@@ -0,0 +1,17 @@
+import ArgumentParser
+
+@main
+struct OfflineASM: ParsableCommand {
+  @Argument(help: "TODO")
+  public var offsetExtractor: String
+  @Argument(help: "TODO")
+  public var output: String
+  @Argument(help: "TODO")
+  public var arch: String
+  @Option(name: .shortAndLong, help: "TODO")
+  public var binary_format: String?
+
+  public func run() throws {
+    print("Hi: ", self.output)
+  }
+}
